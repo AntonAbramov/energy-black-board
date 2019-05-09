2c8144644b8d41face1b656595161f948abc7d1d
anton <antonabramov1990@gmail.com>
2019-05-09 12:12:00 +0200

    remove not used components

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import DashboardPage from './dashboard/DashboardPage';
 import EnergyDetailsPage from './energyDetails/EnergyDetailsPage';
-import NotFound from './components/notFound/notFound';
 
 export const Routes = () => {
   return (
